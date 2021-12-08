2c24df3ebebd5b5448181257861daec04e85c201
desjardins jonathan <jonathan.desjardins@e-artsup.net>
2021-12-08 17:50:17 +0100

    Commencement balancement
    Balancement logiques des plateformes

--- Cloud City/Assets/Jonathan/Scripts/ILES/ile_Poids.cs
+++ Cloud City/Assets/Jonathan/Scripts/ILES/ile_Poids.cs
@@ -8,20 +8,29 @@ public class ile_Poids : MonoBehaviour
 
     public float i_poids = 1f;
 
-    public Vector3 v3rotate = new Vector3(4f,0,0);
+    public Vector3 v3rotate;
+
+    public bool b_ApplyRotation;
+
+    public GameObject gm_Start;
 
     // Start is called before the first frame update
     void Start()
     {
+        v3rotate = new Vector3(i_poids,0,0);
+
+        gm_Start = GameObject.Find("Ville_centrale");
         sc_manager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-       this.gameObject.transform.eulerAngles += v3rotate;
-       Debug.Log(this.gameObject.transform.localEulerAngles);
-       
+        if (b_ApplyRotation)
+        {
+            gm_Start.transform.eulerAngles += v3rotate;
+            Debug.Log(this.gameObject.transform.localEulerAngles);
+            b_ApplyRotation = false;
+        }
     }
 }

30d2b7f8d7111bc1aa8be2214699207e1c1a02a8
Alexandre Montel <alexandre.montel>
2022-02-14 10:20:59 +0100

    404 scene
    recuperation du systeme de placement des modules

---
+++ Cloud City/Assets/404/Export/ScriptsA/CameraScriptAlex.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScriptAlex : MonoBehaviour
+{
+    public Camera cam;
+    Vector3 point;
+    Vector2 mousePos;
+
+    public GameManagerAlex GM;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        point = cam.ScreenToWorldPoint(Input.mousePosition + new Vector3(0f, 0f, cam.nearClipPlane));
+        
+        Raycast();        
+    }
+
+    public void Raycast()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, point - transform.position, out hit)) //le raycast touche quelque chose
+        {
+            if (hit.collider.gameObject.tag == "case" &&
+                hit.collider.gameObject.GetComponent<Sc_CaseAlex>().Dead == false && //verifie que la case est vivante
+                hit.collider.gameObject.GetComponent<Sc_CaseAlex>().IsOverlap == false &&//vérifie que la case n'est pas déjà sous la souris
+                hit.collider.gameObject.GetComponent<Sc_CaseAlex>().OQP == false //vérifie que la case n'est pas déjà Occupé
+                )
+            {
+                GM.Selection (hit.collider.gameObject);
+            }            
+        }
+        else
+        {
+            GM.Selection(null);
+        }
+    }
+
+
+}
---
+++ Cloud City/Assets/404/Export/ScriptsA/VoisinAlex.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoisinAlex : MonoBehaviour
+{
+    public bool disponible, setVoisin, dead; //disponibe, vérifie si le voisin est une case libre ou occ
+
+    public  GameObject voisin, Case, model;
+    public Collider[] Col;
+
+
+    int listposition;    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        disponible = false;
+        setVoisin = true;
+        
+        SetVoisin();
+
+        dead = true;
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    
+    public void MakeDisponible()
+    {
+        disponible = true;
+
+        if (voisin != null)
+        {
+            voisin.GetComponent<Sc_CaseAlex>().Undead();
+            voisin.GetComponent<Sc_CaseAlex>().AddConexion(Case);
+        }
+    }
+
+    public void ModelOn()
+    {
+        model.SetActive(true);
+    }
+    public void ModelOff()
+    {
+        model.SetActive(false);
+    }
+
+    public void GetVoisin()
+    {
+        SetVoisin();
+            
+        if (!Case.GetComponent<Sc_CaseAlex>().list_Voisin.Contains(voisin))
+        {
+            Case.GetComponent<Sc_CaseAlex>().addVoisin(voisin);
+        }
+
+    }
+
+    private void SetVoisin()
+    {
+        Col = Physics.OverlapBox(transform.position, transform.localScale);
+        
+        if (Col.Length > 0)
+        {
+            voisin = Col[0].gameObject;
+        }
+
+    }
+
+
+    public void Bomb()
+    {
+
+    }
+}

20398beeb36e0fd6c9ced03851213f33466999e0
Alexandre Montel <alexandre.montel>
2022-05-16 16:21:20 +0200

    loilol
    lol

--- Cloud City/Assets/Sc_Score.cs
+++ Cloud City/Assets/Sc_Score.cs
@@ -12,11 +12,7 @@ public class Sc_Score : MonoBehaviour
     int score;
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
